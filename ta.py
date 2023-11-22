d42574fca0790a61f9044aa5538ebf732ee6d1ec
BAB78 <146398248+BAB78@users.noreply.github.com>
2023-11-22 16:53:51 +0000

    Update ta.py

--- ta.py
+++ ta.py
@@ -1,4 +1,5 @@
 from netmiko import ConnectHandler
+from netmiko.ssh_exception import NetmikoTimeoutException, NetmikoAuthenticationException
 
 # Define the router details
 router = {
@@ -7,95 +8,81 @@ router = {
     'username': 'cisco',
     'password': 'cisco123!',
     'secret': 'class123!',
+    'port': 22,  # Specify the SSH port if it's different from the default (22)
+    'timeout': 10,  # Adjust timeout as needed
 }
 
-# Connect to the router
-print("Connecting to the router...")
-output = net_connect = ConnectHandler(**router)
-output = net_connect.enable()
-print("Connection established!")
+try:
+    # Connect to the router
+    print("Connecting to the router...")
+    net_connect = ConnectHandler(**router)
+    net_connect.enable()
+    print("Connection established!")
 
-# Configure interfaces with IP addresses
-interface_commands = [
-    'interface Loopback0',
-    'ip address 10.0.0.1 255.255.255.255',
-]
+    # Configure interfaces with IP addresses
+    interface_commands = [
+        'interface Loopback0',
+        'ip address 10.0.0.1 255.255.255.255',
+    ]
 
-print("Configuring interfaces...")
-output = net_connect.send_config_set(interface_commands)
-print("Interfaces configured!")
+    print("Configuring interfaces...")
+    output = net_connect.send_config_set(interface_commands)
+    print("Interfaces configured!")
 
-# Configure OSPF
-ospf_commands = [
-    'router ospf 1',
-    'network 10.0.0.0 255.255.255 area 0',
-    'network 192.168.56.0 0.0.0.255 area 0',
-]
+    # Configure OSPF
+    ospf_commands = [
+        'router ospf 1',
+        'network 10.0.0.0 0.255.255.255 area 0',
+        'network 192.168.56.0 0.0.0.255 area 0',
+    ]
 
-print("Configuring OSPF...")
-output = net_connect.send_config_set(ospf_commands)
-print("OSPF configured!")
+    print("Configuring OSPF...")
+    output = net_connect.send_config_set(ospf_commands)
+    print("OSPF configured!")
 
-# Configure ACLs
-acl_commands = [
-    'access-list 101 permit tcp host 192.168.56.101 any eq www',
-    'access-list 101 permit ip any host 192.168.56.30',
-    'interface GigabitEthernet0/0',
-    'ip access-group 101 in',
-    'ip access-group 101 out',
-    
-]
+    # Configure ACLs
+    acl_commands = [
+        'access-list 101 permit tcp host 192.168.56.101 any eq www',
+        'access-list 101 permit ip any host 192.168.56.30',
+        'interface GigabitEthernet0/0',
+        'ip access-group 101 in',
+        'ip access-group 101 out',
+    ]
 
-print("Configuring ACLs...")
-output =  net_connect.send_config_set(acl_commands)
-print(output)
+    print("Configuring ACLs...")
+    output = net_connect.send_config_set(acl_commands)
+    print("ACLs configured!")
 
-# Configure IPSec
-ipsec_commands = [
-    'crypto isakmp policy 1',
-    'encryption aes',
-    'authentication pre-share',
-    'group 2',
-    'crypto isakmp key your_shared_key address 0.0.0.0',
-    'crypto ipsec transform-set myset esp-aes esp-sha-hmac',
-    'crypto map mymap 10 ipsec-isakmp',
-    'set peer 192.168.56.30',
-    'set transform-set myset',
-    'match address 101',  # Use the ACL number defined in acl_commands
-    'interface GigabitEthernet0/0',
-    'crypto map mymap',
-]
+    # Configure IPSec
+    ipsec_commands = [
+        'crypto isakmp policy 1',
+        'encryption aes',
+        'authentication pre-share',
+        'group 2',
+        'crypto isakmp key your_shared_key address 0.0.0.0',
+        'crypto ipsec transform-set myset esp-aes esp-sha-hmac',
+        'crypto map mymap 10 ipsec-isakmp',
+        'set peer 192.168.56.30',
+        'set transform-set myset',
+        'match address 101',  # Use the ACL number defined in acl_commands
+        'interface GigabitEthernet0/0',
+        'crypto map mymap',
+    ]
 
-print("Configuring IPSec...")
-output = net_connect.send_config_set(ipsec_commands)
-print(output)
+    print("Configuring IPSec...")
+    output = net_connect.send_config_set(ipsec_commands)
+    print("IPSec configured!")
 
-# Disconnect from the router
-print("Disconnecting from the router...")
-output = net_connect.disconnect()
-print("Disconnected!")
+    # Disconnect from the router
+    print("Disconnecting from the router...")
+    net_connect.disconnect()
+    print("Disconnected!")
 
+except NetmikoAuthenticationException as auth_error:
+    print(f"Authentication failed: {auth_error}")
 
-error message 
-Connecting to the router...
-Traceback (most recent call last):
-  File "/home/devasc/.local/lib/python3.8/site-packages/netmiko/base_connection.py", line 899, in establish_connection
-    self.remote_conn_pre.connect(**ssh_connect_params)
-  File "/home/devasc/.local/lib/python3.8/site-packages/paramiko/client.py", line 368, in connect
-    raise NoValidConnectionsError(errors)
-paramiko.ssh_exception.NoValidConnectionsError: [Errno None] Unable to connect to port 22 on 192.168.56.101
+except NetmikoTimeoutException as timeout_error:
+    print(f"Connection to device timed out: {timeout_error}")
 
-During handling of the above exception, another exception occurred:
-
-Traceback (most recent call last):
-  File "/home/devasc/labs/prne/task.py", line 14, in <module>
-    output = net_connect = ConnectHandler(**router)
-  File "/home/devasc/.local/lib/python3.8/site-packages/netmiko/ssh_dispatcher.py", line 266, in ConnectHandler
-    return ConnectionClass(*args, **kwargs)
-  File "/home/devasc/.local/lib/python3.8/site-packages/netmiko/base_connection.py", line 327, in __init__
-    self._open()
-  File "/home/devasc/.local/lib/python3.8/site-packages/netmiko/base_connection.py", line 332, in _open
-    self.establish_connection()
-  File "/home/devasc/.local/lib/python3.8/site-packages/netmiko/base_connection.py", line 905, in establish_connection
-    raise NetmikoTimeoutException(msg)
-netmiko.ssh_exception.NetmikoTimeoutException: Connection to device timed-out: cisco_ios 192.168.56.101:22
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
