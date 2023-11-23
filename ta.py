18c76e308a274d9fc7437c9fbfe4c83d00650a8b
BAB78 <146398248+BAB78@users.noreply.github.com>
2023-11-23 18:27:19 +0000

    Update ta.py

--- ta.py
+++ ta.py
@@ -1,5 +1,33 @@
+Traceback (most recent call last):
+  File "/home/devasc/.local/lib/python3.8/site-packages/paramiko/channel.py", line 699, in recv
+    out = self.in_buffer.read(nbytes, self.timeout)
+  File "/home/devasc/.local/lib/python3.8/site-packages/paramiko/buffered_pipe.py", line 164, in read
+    raise PipeTimeout()
+paramiko.buffered_pipe.PipeTimeout
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "/home/devasc/.local/lib/python3.8/site-packages/netmiko/base_connection.py", line 550, in _read_channel_expect
+    new_data = self.remote_conn.recv(MAX_BUFFER)
+  File "/home/devasc/.local/lib/python3.8/site-packages/paramiko/channel.py", line 701, in recv
+    raise socket.timeout()
+socket.timeout
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "/home/devasc/labs/prne/task.py", line 44, in <module>
+    net_connect.send_config_set(acl_commands)
+  File "/home/devasc/.local/lib/python3.8/site-packages/netmiko/base_connection.py", line 1757, in send_config_set
+    new_output = self.read_until_pattern(pattern=re.escape(cmd.strip()))
+  File "/home/devasc/.local/lib/python3.8/site-packages/netmiko/base_connection.py", line 627, in read_until_pattern
+    return self._read_channel_expect(*args, **kwargs)
+  File "/home/devasc/.local/lib/python3.8/site-packages/netmiko/base_connection.py", line 560, in _read_channel_expect
+    raise NetmikoTimeoutException(
+netmiko.ssh_exception.NetmikoTimeoutException: Timed-out reading channel, data not available.
+
 from netmiko import ConnectHandler
-from netmiko.ssh_exception import NetmikoTimeoutException, NetmikoAuthenticationException
 
 # Define the router details
 router = {
@@ -7,58 +35,60 @@ router = {
     'ip': '192.168.56.101',
     'username': 'cisco',
     'password': 'cisco123!',
+    'secret': 'class123!',
 }
 
-try:
-    # Connect to the router
-    print("Connecting to the router...")
-    net_connect = ConnectHandler(**router)
-    net_connect.enable()
-    print("Connection established!")
+# Connect to the router
+net_connect = ConnectHandler(**router)
+net_connect.enable()
 
-    # i. Configure Access Control Lists (ACLs)
-    acl_commands = [
-        'access-list 101 permit tcp host 192.168.1.100 any eq www',
-        'access-list 101 permit ip any host 192.168.1.50',
-        'interface GigabitEthernet0/1',
-        'ip access-group 101 in',
-        'ip access-group 101 out',
-    ]
+# i. Configure interfaces with IP addresses
+interface_commands = [
+    'interface Loopback0',
+    'ip address 10.0.0.1 255.255.255.255',
+    'interface GigabitEthernet0/0',
+    'ip address 192.168.56.101 255.255.255.0',
+]
 
-    print("Configuring ACLs...")
-    output = net_connect.send_config_set(acl_commands)
-    print("ACLs configured!")
+net_connect.send_config_set(interface_commands)
 
-    # ii. Implement IPSec
-    ipsec_commands = [
-        'crypto isakmp policy 1',
-        'encryption aes',
-        'authentication pre-share',
-        'group 2',
-        'crypto isakmp key strongkey address 0.0.0.0',
-        'crypto ipsec transform-set myset esp-aes esp-sha-hmac',
-        'crypto map mymap 10 ipsec-isakmp',
-        'set peer 192.168.1.50',
-        'set transform-set myset',
-        'match address 101',  # Use the ACL number defined above
-        'interface GigabitEthernet0/1',
-        'crypto map mymap',
-    ]
+# ii. Configure OSPF
+ospf_commands = [
+    'router ospf 1',
+    'network 10.0.0.0 0.255.255.255 area 0',
+    'network 192.168.56.0 0.0.0.255 area 0',
+]
 
-    print("Configuring IPSec...")
-    output = net_connect.send_config_set(ipsec_commands)
-    print("IPSec configured!")
+net_connect.send_config_set(ospf_commands)
 
-    # Disconnect from the router
-    print("Disconnecting from the router...")
-    net_connect.disconnect()
-    print("Disconnected!")
+# iii. Configure ACLs
+acl_commands = [
+    'access-list 101 permit tcp host 192.168.56.101 any eq www',
+    'access-list 101 permit ip any host 192.168.56.30',
+    'interface GigabitEthernet0/0',
+    'ip access-group 101 in',
+    'ip access-group 101 out',
+]
 
-except NetmikoAuthenticationException as auth_error:
-    print(f"Authentication failed: {auth_error}")
+net_connect.send_config_set(acl_commands)
 
-except NetmikoTimeoutException as timeout_error:
-    print(f"Connection to device timed out: {timeout_error}")
+# iv. Configure IPSec
+ipsec_commands = [
+    'crypto isakmp policy 1',
+    'encryption aes',
+    'authentication pre-share',
+    'group 2',
+    'crypto isakmp key your_shared_key address 0.0.0.0',
+    'crypto ipsec transform-set myset esp-aes esp-sha-hmac',
+    'crypto map mymap 10 ipsec-isakmp',
+    'set peer 192.168.56.30',
+    'set transform-set myset',
+    'match address 101',  # Use the ACL number defined in acl_commands
+    'interface GigabitEthernet0/0',
+    'crypto map mymap',
+]
 
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
+net_connect.send_config_set(ipsec_commands)
+
+# Disconnect from the router
+net_connect.disconnect()
